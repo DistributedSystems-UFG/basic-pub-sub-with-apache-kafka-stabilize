0f4ff9e8188a8e6fe7c42f202c2c523fd7091fd2
amanda <8lobogomes@gmail.com>
2022-12-04 22:12:44 -0300

    first commit

--- consumer.py
+++ consumer.py
@@ -1,14 +1,17 @@
 from kafka import KafkaConsumer
 from const import *
-import sys
+import kafka
 
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+admin_client = kafka.KafkaAdminClient(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
 try:
-  topic = sys.argv[1]
+  topics = admin_client.list_topics();
+
 except:
   print ('Usage: python3 consumer <topic_name>')
   exit(1)
   
-consumer.subscribe([topic])
+consumer.subscribe(topics)
 for msg in consumer:
-    print (msg.value)
+    print (msg.value.decode())
--- producer.py
+++ producer.py
@@ -1,17 +1,21 @@
 from kafka import KafkaProducer
 from const import *
-import sys
+import kafka
+
+admin_client = kafka.KafkaAdminClient(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
 try:
-    topic = sys.argv[1]
+    topics = admin_client.list_topics()
+
 except:
     print ('Usage: python3 producer <topic_name>')
     exit(1)
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+
+for topic in topics:
+    msg = 'Hello! my ' + topic + ' topic';
+    print('Sending message: ' + msg);
+    producer.send(topic, value=msg.encode());
 
 producer.flush()
